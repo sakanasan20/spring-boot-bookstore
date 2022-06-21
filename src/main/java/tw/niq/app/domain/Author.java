32a08e7b3ec2b46ac2e5e2e475a7365604ac5df0
niq <sakanasan20@gmail.com>
2022-06-21 09:53:05 +0800

    Create author and book JPA model (#1)

---
+++ src/main/java/tw/niq/app/domain/Author.java
@@ -0,0 +1,37 @@
+package tw.niq.app.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Author {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String firstName;
+	
+	private String lastName;
+	
+	@ToString.Exclude
+	@EqualsAndHashCode.Exclude
+	@ManyToMany(mappedBy = "authors")
+	private Set<Book> books = new HashSet<>();
+	
+}
