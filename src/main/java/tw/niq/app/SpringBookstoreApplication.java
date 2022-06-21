3c7d45e650a619273f0583ad81321c78f8fdd43e
nick.chen <sakanasan20@gmail.com>
2022-06-21 09:28:31 +0800

    Initial commit

---
+++ src/main/java/tw/niq/app/SpringBookstoreApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBookstoreApplication.class, args);
+	}
+
+}
